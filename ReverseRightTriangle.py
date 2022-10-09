390c6633810a70480621ce76076ae318c26bc934
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2022-10-09 09:16:23 +0530

    Reverse Right Triangle

---
+++ ReverseRightTriangle.py
@@ -0,0 +1,12 @@
+# input a intenger from user
+n=int(input())
+
+# making and printing pattern using nested for loop
+for i in range(n):
+  # custom variable for patterns
+  pattern=""
+  for j in range(n-i):
+    pattern+="*"
+    
+  # printing pattern
+  print(pattern)
